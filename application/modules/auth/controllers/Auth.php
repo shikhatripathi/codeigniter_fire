8cc11448af405e70818d9a91a1ef497c1dff5e25
Aman <amankk92@hotmail.com>
2019-01-08 14:56:06 +0530

    auth error

--- application/modules/auth/controllers/Auth.php
+++ application/modules/auth/controllers/Auth.php
@@ -19,7 +19,30 @@ class Auth extends MX_Controller {
 			$data[$value['name']] = $value['value'];
 
 		}
-		//var_dump($data); exit;
+		
+		$this->load->model("auth_model");
+        $user=$this->auth_model->verify_user($data['username'],$data['password']);
+
+       // echo "string";
+       //var_dump($user);
+       //exit;
+        if($user){
+          
+            $session_data = array(
+                'user_id'       =>  $user['user_id'],
+                'user_name'     =>  $user['user_name'],
+                'user_email'    =>  $user['user_email'],
+                'user_type'     =>  $user['user_type']
+            );
+
+
+            $this->session->set_userdata('session_data', $session_data);
+            redirect('home');
+        }else{
+
+            redirect('auth');
+        }
+
 		
 		
 
---
+++ application/modules/auth/models/Auth_model.php
@@ -0,0 +1,24 @@
+<?php
+class Auth_model extends CI_model{
+ 
+ 
+ 
+		
+	
+		function verify_user($email, $password){
+        $this->db->select('*');
+        $this->db->where('user_email', $email);
+        $this->db->where('user_password', $password);
+        $q = $this->db->get('user_reg');
+         
+        if($q->num_rows()>0){
+            return $q->row_array();
+        
+        }else{
+            return false;
+        }
+    }// end of function verify user
+	
+
+		
+}
